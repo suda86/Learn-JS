1f3c733812974442814efb1ebca9b2d821aee5e0
Milos1988 <vojvoda1988>
2022-11-28 07:34:37 +0100

    d2p1

--- day2/part1/chellange/index.js
+++ day2/part1/chellange/index.js
@@ -1,5 +1,11 @@
-const day2part1Chellange = () => {
+const {readFileSync} = require('fs')
+const path = require('path')
+const { day2part1 } = require('..')
 
+const day2part1Chellange = () => {
+    const input = readFileSync(path.resolve(__dirname, 'input.txt'), 'utf8')
+
+    return day2part1(input)
 }
 
 module.exports = {
--- day2/part1/index.js
+++ day2/part1/index.js
@@ -1,7 +1,20 @@
 const day2part1 = (input) => {
-    
-}
+  //   console.log([...input]);
+
+  const newArray = input.split("x");
+
+  console.log(newArray);
+  let nam1 = parseInt(newArray[0] * newArray[1]);
+  //   console.log(nam1);
+  let nam2 = parseInt(newArray[1] * newArray[2]);
+  //   console.log(nam2);
+  let nam3 = parseInt(newArray[2] * newArray[0]);
+  //   console.log(nam3);
+  let min = Math.min(nam1, nam2, nam3);
+
+  return 2 * nam1 + 2 * nam2 + 2 * nam3 + min;
+};
 
 module.exports = {
-    day2part1,
-}
+  day2part1,
+};
