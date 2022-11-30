78ccc46f04d210941140bcd60b8359c6969c923f
Milos1988 <vojvoda1988@gmail.com>
2022-11-30 13:14:26 +0100

    d3p1

--- day3/part1/index.js
+++ day3/part1/index.js
@@ -1,5 +1,43 @@
 const day3part1 = (input) => {
+    let inp = [...input]
+    let gift = 1;
+    let matrix = [];
+    let x = inp.length;
+    let y = inp.length
+
+    function makeMatrix(){
+        for (let i = 0; i < (inp.length * 2 + 1); i++) {
+            matrix.push([])
+            for (let j = 0; j < (inp.length * 2 + 1); j++) {
+                matrix[i].push(1)
+            }
+        }
     
+    }
+
+    makeMatrix()    
+
+    inp.forEach(e => {
+        matrix[x][y] = 0
+        if (e === ">") {
+            gift += matrix[x][y + 1]
+            matrix[x][y + 1] = 0
+            y++
+        } else if (e === "<") {
+            gift += matrix[x][y - 1]
+            matrix[x][y - 1] = 0
+            y--
+        } else if (e === "^") {
+            gift += matrix[x - 1][y]
+            matrix[x - 1][y] = 0
+            x--
+        } else {
+            gift += matrix[x + 1][y]
+            matrix[x + 1][y] = 0
+            x++
+        }
+    })
+
 }
 
 module.exports = {
