794951795f78208c11e548adcfb2846aa599f361
Milos1988 <vojvoda1988@gmail.com>
2022-12-01 08:07:15 +0100

    d3p2 finish

--- day3/part2/index.js
+++ day3/part2/index.js
@@ -1,30 +1,27 @@
 const day3part2 = (input) => {
 
     let inp = [...input]
-    let gift = 0;
+    let gift = 1;
     let matrix = [];
-    
-    let inp1 = inp.filter((el, index) => index % 2  === 0);
-    let inp2 = inp.filter((el, index) => index % 2  !== 0);
-    
-    
-    
-    function makeMatrix(array){
 
-        let x = inp1.length;
-        let y = inp1.length
+    let inp1 = inp.filter((el, index) => index % 2 === 0);
+    let inp2 = inp.filter((el, index) => index % 2 !== 0);
 
-        for (let i = 0; i < (array.length * 2 + 1); i++) {
+    let x1 = inp1.length;
+    let y1 = inp1.length
+    let x2 = inp1.length;
+    let y2 = inp1.length
+
+    function makeMatrix() {
+        for (let i = 0; i < (inp1.length * 2 + 1); i++) {
             matrix.push([])
-            for (let j = 0; j < (array.length * 2 + 1); j++) {
+            for (let j = 0; j < (inp1.length * 2 + 1); j++) {
                 matrix[i].push(1)
             }
         }
-    
     }
 
-    function  addGift(array ){
-
+    function addGift(array, x, y) {
         array.forEach(e => {
             matrix[x][y] = 0
             if (e === ">") {
@@ -47,15 +44,12 @@ const day3part2 = (input) => {
         })
     }
 
-    makeMatrix(inp1)    
-    makeMatrix(inp2)    
+    makeMatrix()
+    addGift(inp1, x1, y1)
+    addGift(inp2, x2, y2)
 
-    addGift(inp1)
-    addGift(inp2)
+    return gift;
 
-    
-    return gift
-    
 }
 
 module.exports = {
