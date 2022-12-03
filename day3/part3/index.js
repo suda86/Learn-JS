566125349157a5b5d20bc4cf1b59951e5c304496
Milos1988 <vojvoda1988>
2022-12-03 12:21:49 +0100

    d3p3-p1

--- day3/part3/index.js
+++ day3/part3/index.js
@@ -1,63 +1,45 @@
 const day3part3 = (input) => {
+  let inp = [...input];
 
-    let inp = [...input]
-    
-    let matrix = [];
-    let x = inp.length;
-    let y = inp.length
+  let matrix = [];
+  let x = inp.length;
+  let y = inp.length;
+  let numberGift = 2;
 
-    function makeMatrix() {
-        for (let i = 0; i < (inp.length * 2 + 1); i++) {
-            matrix.push([])
-            for (let j = 0; j < (inp.length * 2 + 1); j++) {
-                matrix[i].push(0)
-            }
-        }
-
+  function makeMatrix() {
+    for (let i = 0; i < inp.length * 2 + 1; i++) {
+      matrix.push([]);
+      for (let j = 0; j < inp.length * 2 + 1; j++) {
+        matrix[i].push(0);
+      }
     }
+  }
 
-    makeMatrix()
+  makeMatrix();
 
-    inp.forEach(e => {
-        matrix[x][y] = 1
-        if (e === ">") {
-            
-            matrix[x][y + 1] += 1
-            y++
-            return matrix[x][y] 
-        } else if (e === "<") {
-            matrix[x][y - 1] += 1
-            y--
-            return  matrix[x][y]
-        } else if (e === "^") {
-            // console.log(matrix[x - 1][y]+ "^")
-            // console.log(matrix[x - 1][y]+ "^")
-            matrix[x - 1][y] ++
-            // console.log(matrix[x - 1][y]+ "^")
-            x--
-            return matrix[x][y] 
-        } else {
-            // console.log(matrix[x + 1][y] +"v")
-            matrix[x + 1][y] ++
-            // console.log(matrix[x + 1][y] +"v")
-            x++
-            return matrix[x][y] 
-        }
-    })
+  inp.forEach((e) => {
+    matrix[x][y] = 1;
+    if (e === ">") {
+      matrix[x][y + 1] += 1;
+      y++;
+    } else if (e === "<") {
+      matrix[x][y - 1] += 1;
+      y--;
+    } else if (e === "^") {
+      matrix[x - 1][y] += 1;
+      x--;
+    } else {
+      matrix[x + 1][y] += 1;
+      x++;
+    }
+  });
 
+  let array = [].concat(...matrix);
+  let numberHouseWithTwoGift = array.filter((arr) => arr === numberGift);
 
-
-    let array = [].concat(...matrix);
-    let numberHouseWithTwoGift = array.filter(arr=>arr===2)
-
-
-    
-    
-    return numberHouseWithTwoGift.length
-
-
-}
+  return numberHouseWithTwoGift.length;
+};
 
 module.exports = {
-    day3part3,
-}
+  day3part3,
+};
